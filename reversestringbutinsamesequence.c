fa3b7fe208bb4d0af9e2575e1f3f5365e9e88c8a
Aakrit_Bhandari <122432145+Aakrit-Bhandari@users.noreply.github.com>
2023-03-11 23:11:31 +0530

    fI uoy era elba ot daer siht llew dna .doog sihT edoc lliw esrever a gnirts tub ti lliw tnirp ti ni eht emas redro nevig yb eht .resu
    ____________Input_____________________
    fI uoy era elba ot daer siht llew dna .doog sihT edoc lliw esrever a gnirts tub ti lliw tnirp ti ni eht emas redro nevig yb eht .resu
    _____________________________________
    Let me show you what this does.
    ___________Output_____________________
    If you are able to read this well and good. This code will reverse a string but it will print it in the same order given by the user.

---
+++ reversestringbutinsamesequence.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+int main(void)
+{
+    char a[50];
+    // fgets(a, 50, stdin);
+    scanf("%[^\n]%*c", a);
+    char *g = strtok(a, " ");
+    while (g != '\0')
+    {
+        strrev(g);
+        printf("%s ", g);
+        g = strtok(NULL, " ");
+    }
+}
